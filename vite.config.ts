3a59aba8cac99e8fa325639003c5aa8979076e29
bryzZz <nikita.tebloev@polyus-nt.ru>
2023-08-03 22:08:17 +0900

    add workflow

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  base: "/transcript-admin/",
+  plugins: [svgr(), react()],
+  resolve: {
+    alias: {
+      pages: path.resolve(__dirname, "./src/pages"),
+      components: path.resolve(__dirname, "./src/components"),
+      hooks: path.resolve(__dirname, "./src/hooks"),
+      types: path.resolve(__dirname, "./src/types"),
+      assets: path.resolve(__dirname, "./src/assets"),
+      models: path.resolve(__dirname, "./src/models"),
+      services: path.resolve(__dirname, "./src/services"),
+      constants: path.resolve(__dirname, "./src/constants"),
+      routes: path.resolve(__dirname, "./src/routes"),
+      context: path.resolve(__dirname, "./src/context"),
+      utils: path.resolve(__dirname, "./src/utils"),
+      sections: path.resolve(__dirname, "./src/sections"),
+    },
+  },
+});
